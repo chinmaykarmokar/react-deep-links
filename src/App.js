245dd0d2ad87041528e34be775325ae83d92af81
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2024-11-24 23:54:32 +0530

    Added test route and config change in assetlinks.json

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<>Home</>} />
+					<Route path="/details" element={<>Details</>}/>
 				</Routes>
 			</BrowserRouter>	
 		</>
